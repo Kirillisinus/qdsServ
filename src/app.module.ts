04f8df02765b6d5d3b69fff05aa57ef7e68985f2
Kirill <pimenov549@gmail.com>
2022-04-25 23:51:37 +0300

    start migraion

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EnterGame } from './socket.gateway'
+import { EnterGame } from './socket.gateway';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './user.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'ec2-63-32-248-14.eu-west-1.compute.amazonaws.com',
-      port: 5432,
-      username: 'awjqkgiaynfnrq',
-      password: 'c69dd8fdc4129e39e5375e06c720358b4598d8d55a640fabd3fd59ebbbb2aed8',
-      database: 'd1uvmmelf3brrh',
-      entities: [Users],
-      synchronize: true,
-    }),
-    TypeOrmModule.forFeature([Users])
+    TypeOrmModule.forRoot(),
+    TypeOrmModule.forFeature([Users]),
   ],
   controllers: [AppController],
   providers: [AppService, EnterGame],
