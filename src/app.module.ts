35695779d08d376f2b8843ec6a63a6af8ab350e1
Kirill <pimenov549@gmail.com>
2022-04-26 22:37:16 +0300

    add dotenv file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,24 @@ import { AppService } from './app.service';
 import { EnterGame } from './socket.gateway';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './user.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
-    TypeOrmModule.forFeature([Users]),
+    Users,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(
+      {
+        type: 'postgres',
+        url: process.env.DATABASE_URL,
+        entities: ['dist/**/*.entity{.ts,.js}'],
+        synchronize: false,
+        autoLoadEntities: true,
+        extra: {
+          rejectUnauthorized: false
+        }
+      }
+    )
   ],
   controllers: [AppController],
   providers: [AppService, EnterGame],
