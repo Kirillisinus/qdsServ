929fc3dbc8409df43bce63be67802118e1544899
Kirillisinus <pimenov549@gmail.com>
2022-06-21 17:23:49 +0300

    del comm

--- src/app.service.ts
+++ src/app.service.ts
@@ -41,14 +41,6 @@ export class AppService {
         "SELECT nextval('user_pkid')",
       );
 
-      /*const user = new Users();
-      user.id=nextId[0].nextval;
-      user.user=name;
-      user.exp_date = current ;
-      user.in_game=false;
-      user.in_lobby=false;
-      user.is_admin=false;*/
-
       await this.usersRepository.query(
         'INSERT INTO users(id, "user", exp_date, in_lobby, in_game, is_admin) VALUES ($1, $2, $3, $4, $5, $6)',
         [nextId[0].nextval, name, current, false, false, false],
@@ -86,21 +78,13 @@ export class AppService {
     const users = await this.usersRepository.query(
       'SELECT * FROM users as u WHERE u.in_game=true AND u.socket_id IS NOT NULL AND u.exp_date > CURRENT_DATE ORDER BY u.id DESC;',
     );
-    // console.log(users);
     const rounds = await this.gameRepository.query("SELECT creator FROM game_session group by creator order by creator ASC");
-    // console.log(rounds);
     const users_in_session = await this.gameRepository.query("SELECT prev FROM game_session group by prev order by prev ASC");
-    // console.log(users_in_session);
-    // const game_session_for_user1 = await this.gameRepository.query("SELECT * FROM game_session WHERE creator = $1 AND prev = $2 order by creator ASC", [0, users_in_session[0].prev]);
-
     let history_items = [];
-    // history_items.push(game_session_for_user1[0].data);
 
     let history_marker = 0;
 
     let history_obj: History[] = [];
-
-    //history_obj[0] = new History(usr_for_history1[0].user, history_items);
 
     for (let i = 0; i < users_in_session.length; i++) {
       history_marker = users_in_session[i].prev;
@@ -115,8 +99,6 @@ export class AppService {
         };
 
         history_items.push(obj);
-
-        // console.log("Items for user with id " + history_marker + " is " + history_items[j]);
       }
       const usr_for_history = await this.usersRepository.query(
         'SELECT u.user FROM users as u WHERE u.id=$1;', [users_in_session[i].prev]
@@ -131,10 +113,6 @@ export class AppService {
   async whatToDraw(name: string, creator: string): Promise<any> {
     this.usrs = [];
     const usr = await this.usersRepository.find({ where: { user: name } });
-    // console.log(usr);
-    // console.log(creator);
-
-    //if (this.turn) {
     const ids_of_users = await this.usersRepository.query(
       'SELECT u.id FROM users u WHERE u.in_game=true AND u.socket_id IS NOT NULL ORDER BY u.id DESC',
     );
@@ -142,9 +120,7 @@ export class AppService {
     for (let i = 0; i < ids_of_users.length; i++) {
       this.usrs.push(ids_of_users[i].id);
     }
-    //console.log(this.usrs);
     this.turn = false;
-    //}
 
     let crtr_id = 0;
     for (let i = 0; i < ids_of_users.length; i++) {
@@ -159,17 +135,9 @@ export class AppService {
         }
       }
     }
-
-    /*const usr_crtr = await this.usersRepository.find({
-      where: { user: creator },
-    });
-    console.log(usr_crtr);*/
     const thing = await this.gameRepository.findOne({
-      //where: { next: usr[0].id, creator: usr_crtr[0].id },
       where: { next: usr[0].id, creator: creator },
     });
-    //console.log("For client with id " + usr[0].id + " creator(round) was " + creator + " and need do this: " + thing.data);
-    //const ans = { data: thing.data, creator: usr_crtr[0].user };
     const ans = { data: thing.data };
 
     return ans;
--- src/socket.gateway.ts
+++ src/socket.gateway.ts
@@ -90,13 +90,6 @@ export class EnterGame {
     this.logger.log(`Client with id: ${client.id}` + ' left lobby');
   }
 
-  /*@SubscribeMessage('ready')
-  async ready(client: Socket, payload: string): Promise<void> {
-    await this.usersRepository.query("UPDATE users u SET is_ready=true WHERE u.user=$1",[payload]);
-
-    this.logger.log(`Client with id: ${client.id}`+" is ready now");
-  }*/
-
   @SubscribeMessage('startGame')
   async startGame(client: Socket, data: string): Promise<void> {
     this.users = [];
@@ -140,34 +133,16 @@ export class EnterGame {
 
     this.round_now = 0;
 
-    // this.logger.log('users: ' + this.users);
-    // this.logger.log('ready_of_all: ' + this.ready_of_all);
-    // this.logger.log('num_of_rounds: ' + this.num_of_rounds);
-    // this.logger.log('round_now: ' + this.round_now);
-    // this.logger.log('time_of_round: ' + this.time_of_round);
-    // this.logger.log('arr_of_next_pages: ' + this.arr_of_next_pages);
-    // this.logger.log('admin_id: ' + this.admin_id);
-
     this.server.emit('startMsg', this.time_of_round);
 
     if (this.users.length > 1) {
       this.time_of_round += 5 * this.users.length;
     }
-
-    /*this.timerId = setTimeout(() => {
-      this.server.emit('timeIsUp');
-       if (this.timerId) {
-            clearTimeout(this.timerId);
-            this.timerId = 0;
-        }
-    },this.time_of_round*1000);*/
   }
 
   @SubscribeMessage('writeData')
   async writeSentence(client: Socket, data: any) {
     const usr = await this.usersRepository.find({ where: { socket_id: client.id } });
-    //this.logger.log('client: ' + usr[0]);
-    //const nextId = await this.usersRepository.query('SELECT nextval(\'game_session_pkid\')');
     let nextId = 0;
     for (let i = 0; i < this.users.length; i++) {
       if (this.users[i][0] === usr[0].id) {
@@ -176,32 +151,12 @@ export class EnterGame {
       }
     }
 
-    //this.logger.log(data.sentence + " " + data.creator);
-
     const usr_crtr = await this.usersRepository.query("SELECT u.id FROM users as u where u.user=$1", [data.creator]);
-    /*let crtr_id = usr[0].id;
-    if (this.round_now != 0) {
-      
-      for (let i = 0; i < this.users.length; i++) {
-        if (this.users[i][0] === usr[0].id) {
-          if (i - this.round_now < 0) {
-            crtr_id = this.users[this.users.length - 1][0];
-            i = this.users.length;
-          }
-          else {
-            crtr_id = this.users[i - 1][0];
-            i = this.users.length;
-          }
-        }
-      }
-    }*/
 
     const session_row = new gameSession();
-    //session_row.creator = usr_crtr[0].id;
     session_row.creator = this.round_now;
     session_row.prev = usr[0].id;
     session_row.data = data.sentence;
-    //this.logger.log('this.users[' + nextId + '][0]: ' + this.users[nextId][0]);
     session_row.next = this.users[nextId][0];
 
     await this.gameRepository.save(session_row);
@@ -209,36 +164,11 @@ export class EnterGame {
     this.ready_of_all--;
 
     if (this.ready_of_all <= 0) {
-      //clearTimeout(this.timerId);     
       this.ready_of_all = this.users.length;
 
       if (this.users.length > 1) {
         this.time_of_round -= 5;
       }
-
-
-      //this.logger.log('go to ' + this.arr_of_next_pages[this.round_now]);
-      //this.logger.log('Client ' + client.id + ' has written ' + data);
-
-      //this.logger.log('ready_of_all: ' + this.ready_of_all);
-      //this.logger.log('num_of_rounds: ' + this.num_of_rounds);
-     // this.logger.log('round_now: ' + this.round_now);
-     // this.logger.log('time_of_round: ' + this.time_of_round);
-      //this.logger.log('arr_of_next_pages: ' + this.arr_of_next_pages);
-      //this.logger.log('admin_id: ' + this.admin_id);
-
-      /*for (let i = 0; i < this.users.length; i++) {
-        const usr_send = await this.usersRepository.query("SELECT * FROM users as u where u.id=$1", [this.users[i][0]]);
-        let prv = 0;
-        for (let cr = 0; cr < this.users.length; cr++) {
-          if (this.users[this.users[cr][1]][0] === usr_send[0].id)
-            prv = cr;
-        }
-        const usr_send_creator = await this.gameRepository.query("SELECT * FROM game_session as g where g.next=$1 AND g.prev=$2", [this.users[i][0], this.users[prv][0]]);
-        this.server.to(usr_send[0].socket_id).emit('updCreator',usr_send_creator[0].creator);
-      }*/
-
-
 
       this.server.emit('goNextMsg', {
         next_page: this.arr_of_next_pages[this.round_now],
@@ -255,105 +185,11 @@ export class EnterGame {
         }
         this.users[i][1] = variable;
       }
-
-      // this.logger.log('users: ' + this.users);
-      /*this.timerId = setTimeout(() => {
-      this.server.emit('timeIsUp');
-      if (this.timerId) {
-            clearTimeout(this.timerId);
-            this.timerId = 0;
-        }     
-    },this.time_of_round*1000);*/
     }
   }
 
-  /* @SubscribeMessage('whatToWrite')
-   async whatWrite() {
-     this.server.emit('goNextMsg', {
-         next_page: this.arr_of_next_pages[this.round_now],
-         round_time: this.time_of_round,
-       });
-       this.server.emit('timeIsUp');
-   }*/
-
-  /*@SubscribeMessage('drawImage')
-  async drawImage(client: Socket, data: string) {
-    const usr =  await this.usersRepository.find({ where: { socket_id: client.id } });
-    let nextId = 0;
-    for(let i = 0; i < this.users.length; i++){
-        if(this.users[i][0] === usr[0].id) {
-            nextId = this.users[i][1];
-            i=this.users.length;
-        }
-    }
-
-    const session_row = new gameSession();
-    session_row.creator;
-    session_row.prev = usr[0].id;
-    session_row.data = data;
-    session_row.next = nextId;
-
-    await this.gameRepository.save(session_row);
-
-    this.ready_of_all--;
-
-    if (this.ready_of_all <= 0) {
-      //clearTimeout(this.timerId);
-      this.ready_of_all = this.users.length;
-
-      if (this.users.length > 1) {
-        this.time_of_round -= 5;
-      }
-
-      /*this.logger.log('go to ' + this.arr_of_next_pages[this.round_now]);
-      this.logger.log('Client ' + client.id + ' has drawed ' + 'data');
-      this.logger.log('users: ' + this.users);
-      this.logger.log('ready_of_all: ' + this.ready_of_all);
-      this.logger.log('num_of_rounds: ' + this.num_of_rounds);
-      this.logger.log('round_now: ' + this.round_now);
-      this.logger.log('time_of_round: ' + this.time_of_round);
-      this.logger.log('arr_of_next_pages: ' + this.arr_of_next_pages);
-      this.logger.log('admin_id: ' + this.admin_id);
-
-      this.server.emit('goNextMsg', {
-        next_page: this.arr_of_next_pages[this.round_now],
-        round_time: this.time_of_round,
-      });
-      this.round_now++;
-
-      for (let i = 0; i < this.users.length; i++) {
-        let variable = this.users[i][1] + 1;
-        if(variable>=this.users.length){
-            variable = 0;
-        }
-        this.users[i][1] = variable;
-      }
-
-      this.timerId = setTimeout(() => {
-      this.server.emit('timeIsUp');
-      if (this.timerId) {
-            clearTimeout(this.timerId);
-            this.timerId = 0;
-        }
-    },this.time_of_round*1000);
-    }
-  }*/
-
-  /*@SubscribeMessage('whatToDraw')
-  async whatToDraw() {
-    this.server.emit('goNextMsg', {
-        next_page: this.arr_of_next_pages[this.round_now],
-        round_time: this.time_of_round,
-      });
-      this.server.emit('timeIsUp');
-  }*/
-
   @SubscribeMessage('timeIsUp')
   async timeIsUp() {
-    /*this.server.emit('goNextMsg', {
-        next_page: this.arr_of_next_pages[this.round_now],
-        round_time: this.time_of_round,
-      });*/
     this.server.emit('timeIsUp');
   }
 
